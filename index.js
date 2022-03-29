5acdf3aa85e2cdafa457f4731fd5fea579a641ec
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2022-03-29 06:42:36 +0500

    add writing problems to log

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require("express-async-errors")
 const express = require("express");
 const app = express();
+const errorMiddleware = require("./middleware/error")
 const config = require("config");
 const mongoose = require("mongoose");
+const winston = require("winston")
 const userRouter = require("./routes/users")
 const authRouter = require("./routes/auth")
 const postRouter = require("./routes/post")
 
+winston.add(new winston.transports.File({filename: "blog-site-logs.log"}))
 mongoose.connect(`mongodb://${config.get("path")}`)
     .then(() => console.log("MongoDB ga ulanish amalga oshirildi:)"))
     .catch(err => console.log("MongoDB ga ulanish amalga oshmadi:("))
@@ -14,5 +18,6 @@ app.use(express.json())
 app.use("/api/users", userRouter)
 app.use("/api/login", authRouter)
 app.use("/api/posts", postRouter)
+app.use(errorMiddleware)
 
 app.listen(5000,()=> console.log("5000 chi portni eshitishni boshladim"))
